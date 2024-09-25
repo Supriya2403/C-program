ac0b9a8c517712efba2cf33a29e8a4a0847ab114
Supriya2403 <supriyasonij6@gmail.com>
2024-09-25 20:10:22 +0530

    Add files via upload

---
+++ add.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,sum;
+    printf("enter the first number:");
+    scanf("%d",&a);
+    printf("enter the second number:");
+    scanf("%d32",&b);
+    sum=a+b;
+    printf("\n tne sum of given number:%d",sum);
+}
