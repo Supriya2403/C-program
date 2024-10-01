335ae0bdd8eb93eea8152482eab90cb39d8c3739
Supriya2403 <supriyasonij6@gmail.com>
2024-10-01 23:58:57 +0530

    Add files via upload

---
+++ average.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,avg;
+    printf("\n enter a:");
+    scanf(" \n %d",&a);
+    printf(" \n enter b:");
+    scanf(" \n %d",&b);
+    avg=a+b/2;
+    printf(" \n the average number is:%d",avg);
+}
