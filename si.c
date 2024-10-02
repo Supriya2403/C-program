bdf70e9cd6b7f3dd887ae8d35df1ef4a357abca1
Supriya2403 <supriyasonij6@gmail.com>
2024-10-02 21:55:31 +0530

    Add files via upload

---
+++ si.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int si,p,r,t;
+    printf("enter the value of p,r,t");
+    scanf("%d%d%d",&p,&r,&t);
+    si=(p*r*t)/100;
+    printf("the value of si is %d",si);
+    return 0;\
+}
