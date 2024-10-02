373d1d46bc13bb4eace26a182bf992502c52b2c1
Supriya2403 <supriyasonij6@gmail.com>
2024-10-02 21:55:53 +0530

    Add files via upload

---
+++ squareofno.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int a;
+    printf("\n enter the number:");
+    scanf("\n %d",&a);
+    int root=sqrt(a);
+    printf("\n the sqaure root the given number is :%d",root);
+}
