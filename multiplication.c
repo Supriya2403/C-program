4c2e35a7c52ce426fda39214756c85386e80f35e
Supriya2403 <supriyasonij6@gmail.com>
2024-10-05 20:57:34 +0530

    Add files via upload

---
+++ multiplication.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int num,i;
+    printf("\n Enter the number:");
+    scanf("%d",&num);
+ printf(" \n The multiplication of number:%d",num);
+ for(int i=1;i<=10;i++)
+ {
+    printf(" \n %d",num*i);
+
+ }
+
+
+}
