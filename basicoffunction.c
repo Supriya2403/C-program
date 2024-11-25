54241de6fda8aaf0344aab847896c0a00a376ec4
Supriya2403 <supriyasonij6@gmail.com>
2024-11-25 21:27:06 +0530

    Add files via upload

---
+++ basicoffunction.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void greet()
+{
+    printf("\n Good Morning");
+    printf("\n How Are You ?");
+    return;
+}
+int main()
+{
+    greet();
+    greet();
+greet();
+greet();
+greet();
+greet();
+greet();
+
+}
