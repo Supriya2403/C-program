0efaf20c75bc08f2f4f06f9fb99eae7edcd0c6b6
Supriya2403 <supriyasonij6@gmail.com>
2024-11-25 21:37:11 +0530

    Add files via upload

--- sumoftwo.c
+++ sumoftwo.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
-int main(){
-    int i,n,r,sum=0;
-    printf("\n enter the number:");
-    scanf(" \n %d",&n);
-    while (n>0)
-    {
-        r=n%10;
-        sum=sum+r;
-        n=n/10;
-
-    }
-    printf("sum of the digits:%d",sum);
-
+int add(int a,int b){
+return a+b;
+}
+int main()
+{
+    int a;
+    printf("\n enter a:");
+    scanf("\n %d",&a);
+    int b;
+    printf("\n enter b:");
+    scanf("\n %d",&b);
+  int  sum =add(a,b);
+    printf("\n the sum of two numbers is:%d",sum);
 }
